c44854d7452c08f9cd3970135ca6cc16c3a82003
Kaz KOBARA <M-kaz-ml@aist.go.jp>
2021-05-05 08:50:57 +0900

    Update sizeof

--- sizeof.md
+++ sizeof.md
@@ -3,11 +3,13 @@
 ## 必要な背景知識
 
 - c言語
-- ポインタ、構造体
+- ポインタ
 
 ## 型とサイズ
 
-特に long、long long などは処理系で型のサイズが異なるため、sizeof() 関数で確認するためのコードを[こちら](./src/sizeof.c)に置いてある。
+処理系で型のサイズが異なる場合がある(特に long、long long など)ため、各型のサイズを確認するコードを[こちら](./src/sizeof.c)に置いてあります。
+
+構造体のサイズは、条件により変わるため[こちら](struct.md)をご参照下さい。
 
 以下は、
 
@@ -31,18 +33,20 @@ double           : 8
 long double      : 16
 
 int32_t array[9] : 36
-pointer          : 8
+pointer (ptr)    : 8
 int32_t *ptr     : 4
 void *void_ptr   : 1
-
-struct ci16i32   : 8
-struct i16i32c   : 12
 ```
 
-要点としては、
+## 配列のサイズ
 
-- スタック領域上の配列( int32_t array[9]; など)では sizeof(int32_t) x [配列のサイズ] = 4x9 = 36 となるのに対して、ヒープ領域に動的に割り当てた配列( int32_t \*ptr = (int32_t \*)calloc(9, sizeof(int32_t)); など)では __sizeof(ptr) は ptr のポインターのサイズ__、 __sizeof(\*ptr)はptrが指す値の型のサイズ(型がvoidの場合は1オクテット)__ となるため、注意が必要。
-- sizeof(構造体) については、その要素のサイズに応じてアライメントされるため、並びに応じてサイズが変わる場合がある。
+sizeof()の引数により異なるため注意が必要:
+
+- __スタック領域__ 上の配列( int32_t array[9]; など)では [配列の型のサイズ] x [配列のサイズ]
+  - 上記の例では、sizeof(array) = sizeof(int32_t) x [配列のサイズ] = 4x9 = 36。
+- __ヒープ領域__ に動的に割り当てた配列( int32_t \*ptr = (int32_t \*)calloc(9, sizeof(int32_t)); など)
+  - sizeof(ptr) は ptr のポインターのサイズ
+  - sizeof(\*ptr)はptrが指す値の型のサイズ(型がvoidの場合は1オクテット)
 
 ## サイズを考慮した型
 
--- src/sizeof.c
+++ src/sizeof.c
@@ -14,40 +14,22 @@ int32_t main(void)
     void *void_ptr = (int32_t *)calloc(11, sizeof(int32_t));
     const int32_t array[9];
 
-    struct struct1 {
-        int16_t val3;
-        int32_t val1;
-        char    val2;
-    };
-
-    struct struct2 {
-        char    val2;
-        int16_t val3;
-        int32_t val1;
-    };
-
-    const struct struct1 i16i32c;
-    const struct struct2 ci16i32;
-
     (void)printf("\n");
     (void)printf("Type             : Size in Octets \n");
     (void)printf("----------------------------------\n");
-    (void)printf("char             : %lu\n", sizeof(char));
-    (void)printf("short int        : %lu\n", sizeof(short int));
-    (void)printf("int              : %lu\n", sizeof(int));
-    (void)printf("long int         : %lu\n", sizeof(long int));
-    (void)printf("long long int    : %lu\n", sizeof(long long int));
-    (void)printf("float            : %lu\n", sizeof(float));
-    (void)printf("double           : %lu\n", sizeof(double));
-    (void)printf("long double      : %lu\n", sizeof(long double));
+    (void)printf("char             : %zu\n", sizeof(char));
+    (void)printf("short int        : %zu\n", sizeof(short int));
+    (void)printf("int              : %zu\n", sizeof(int));
+    (void)printf("long int         : %zu\n", sizeof(long int));
+    (void)printf("long long int    : %zu\n", sizeof(long long int));
+    (void)printf("float            : %zu\n", sizeof(float));
+    (void)printf("double           : %zu\n", sizeof(double));
+    (void)printf("long double      : %zu\n", sizeof(long double));
     (void)printf("\n");
-    (void)printf("int32_t array[9] : %lu\n", sizeof(array));
-    (void)printf("pointer (ptr)    : %lu\n", sizeof(ptr));
-    (void)printf("int32_t *ptr     : %lu\n", sizeof(*ptr));
-    (void)printf("void *void_ptr   : %lu\n", sizeof(*void_ptr));
-    (void)printf("\n");
-    (void)printf("struct ci16i32   : %lu\n", sizeof(ci16i32));
-    (void)printf("struct i16i32c   : %lu\n", sizeof(i16i32c));
+    (void)printf("int32_t array[9] : %zu\n", sizeof(array));
+    (void)printf("pointer (ptr)    : %zu\n", sizeof(ptr));
+    (void)printf("int32_t *ptr     : %zu\n", sizeof(*ptr));
+    (void)printf("void *void_ptr   : %zu\n", sizeof(*void_ptr));
     (void)printf("\n");
 
     free(ptr);
